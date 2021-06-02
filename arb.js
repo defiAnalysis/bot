34927a59f5747ed8e8dc2ef142059ea95ef236f1
hayden <cfduan@outbook.com>
2021-06-02 19:56:35 +0800

    update

--- arb.js
+++ arb.js
@@ -174,8 +174,82 @@ setInterval(function(){
         doSwap();
     }
 },3000)
+/*
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
 
+interface Router{
+    function getAmountsOut(uint amountIn, address[] memory path) external view  returns (uint[] memory amounts);
+    //okt --> usdt
+     function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline)
+        external
+        payable
+        returns (uint[] memory amounts);
 
+    //usdk --> okt
+    function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline)
+        external
+        returns (uint[] memory amounts);
+        
+    function WETH() external pure returns (address);
+}
 
-//balance 6.6801
-//0x6AED11801Aae0C43E31448B7209acF77593b30d2
+contract arb {
+    address usdt;
+    //king kong swap route address
+    address kks;
+    //kswap route address
+    address ksp;
+    
+    address kks_wokt;
+    
+    address ksp_wokt;
+    
+    address owner;
+    
+    event BuyUSDT(address to,uint amountIn,uint amountOut);
+    event sellUSDT(address to,uint amountIn,uint amountOut);
+
+    constructor() public {
+        usdt = 0xe579156f9dEcc4134B5E3A30a24Ac46BB8B01281;
+
+        kks = 0xD9Ee582C00E2f6b0a5A0F4c18c88a30e49C0304b;
+
+        ksp = 0x2f46e5fF1F616cfc00F4e6fA2eFFbA4B0AAA7b6F;
+        
+        kks_wokt= 0x70c1c53E991F31981d592C2d865383AC0d212225;
+        
+        ksp_wokt = 0x2219845942d28716c0F7C605765fABDcA1a7d9E0;
+        
+        owner = msg.sender;
+        
+    }
+    
+    modifier onlyOwner() {
+        require(
+            msg.sender==owner,
+            "sender not owner"
+        );
+        _;
+    }
+    
+    function arbChange() public payable {
+        uint deadline = block.timestamp + 1000 * 60 * 10;
+        
+        address[] memory buy_path = new address[](2);
+        buy_path[0] = kks_wokt;
+        buy_path[1] = usdt;
+        uint[] memory amountsBuy = Router(kks).swapExactETHForTokens{value: msg.value}(0,buy_path,msg.sender,deadline);
+        emit BuyUSDT(msg.sender,amountsBuy[amountsBuy.length-2],amountsBuy[amountsBuy.length-1]);
+        
+        address[] memory sell_path = new address[](2);
+        buy_path[0] = usdt;
+        buy_path[1] = ksp_wokt;
+        uint[] memory amountsSell = Router(ksp).swapExactTokensForETH(amountsBuy[amountsBuy.length-1],0,sell_path,msg.sender,deadline);
+        emit sellUSDT(msg.sender,amountsSell[amountsSell.length-2],amountsSell[amountsSell.length-1]);
+    }
+
+   
+}
+
+*/
