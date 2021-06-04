2b409e0d75f9c1cfe4dd57a7be3c51a1ae14f028
hayden <cfduan@outbook.com>
2021-06-04 19:01:09 +0800

    update

--- arbSwap.sol
+++ arbSwap.sol
@@ -23,6 +23,9 @@ contract ARB {
     address internal owner;
     
     event Received(address sender, uint amount);
+    event Buy(address sender,address router,uint amountIn,uint amountOut);
+    event Sell(address sender,address router,uint amountIn,uint amountOut);
+    event BuyAndSell(uint amountIn,uint amountOut);
     
     constructor() {
         owner = msg.sender; // 'msg.sender' is sender of current call, contract deployer for a constructor
@@ -35,27 +38,39 @@ contract ARB {
         }
         
         
-    function EthToTokens(address router,address tokenAddress,uint amountOutMin,uint deadline) public payable {
+    function EthToTokensWithPaths(address router,address tokenAddress,address[] memory paths,uint amountIn,uint amountOutMin,uint deadline)
+        public 
+        payable
+         returns (uint[] memory amounts)
+        {
        // uint deadline = block.timestamp + 10 *60;
- 
-        IUniswapV2Router02(router).swapExactETHForTokens{ value: msg.value }(amountOutMin, getPathForEthToToken(router,tokenAddress), address(this), deadline);
+            
+         amounts = IUniswapV2Router02(router).swapExactETHForTokens{ value: amountIn }(amountOutMin, paths, address(this), deadline);
+        
+         emit Buy(msg.sender,router,amounts[0],amounts[amounts.length-1]);    
         }
     
-    function TokensToEth(address router,address tokenAddress, uint TokenInAmount,uint amountOutMin,uint deadline) public payable {
+    function TokensToEthWithPaths(address router,address tokenAddress, address[] memory paths,uint TokenInAmount,uint amountOutMin,uint deadline) 
+        public 
+        payable 
+        returns (uint[] memory amounts)
+        {
        //uint deadline = block.timestamp + 10 *60;
         IERC20 token = IERC20(tokenAddress);
         token.approve(router, TokenInAmount);
-        IUniswapV2Router02(router).swapExactTokensForETH(TokenInAmount, amountOutMin, getPathForTokenToEth(router,tokenAddress), address(this), deadline);
+        amounts = IUniswapV2Router02(router).swapExactTokensForETH(TokenInAmount, amountOutMin, getPathForTokenToEth(router,tokenAddress), address(this), deadline);
+        
+        emit Sell(msg.sender,router,amounts[0],amounts[amounts.length-1]); 
         }
     
-    function getPathForEthToToken(address router,address tokenAddress) private pure returns (address[] memory) {
+    function getPathForEthToToken(address router,address tokenAddress) public pure returns (address[] memory) {
         address[] memory path = new address[](2);
         path[0] = IUniswapV2Router02(router).WETH();
         path[1] = tokenAddress;
         return path;
         }    
         
-    function getPathForTokenToEth(address router,address tokenAddress) private pure returns (address[] memory) {
+    function getPathForTokenToEth(address router,address tokenAddress) public pure returns (address[] memory) {
         address[] memory path = new address[](2);
         path[0] = tokenAddress;
         path[1] = IUniswapV2Router02(router).WETH();
@@ -79,7 +94,9 @@ contract ARB {
        // uint deadline = block.timestamp + 10 *60;
  
         IKSWPRouter(router).swapExactETHForTokens{ value: msg.value }(amountOutMin, kswpgetPathForEthToToken(router,tokenAddress), address(this), deadline);
-        }
+        
+        
+    }
     
     function KSWPTokensToEth(address router,address tokenAddress, uint TokenInAmount,uint amountOutMin,uint deadline) public payable {
        //uint deadline = block.timestamp + 10 *60;
@@ -101,6 +118,13 @@ contract ARB {
         path[1] = IKSWPRouter(router).WOKT();
         return path;
         }
+        
+    function ArbBuyAndSell(address[] memory _routers,address tokenAddress,address[] memory inPaths,address[] memory outPaths,uint deadline) 
+        public payable isOwner {
+           uint[] memory amounts = EthToTokensWithPaths(_routers[0],tokenAddress,inPaths,msg.value,0,deadline);
 
-
+            uint[] memory sellAmounts = TokensToEthWithPaths(_routers[1],tokenAddress,outPaths,amounts[amounts.length-1],0,deadline);
+           //require(amounts[0] - sellAmounts[1] > 0.02 gwei);
+            emit BuyAndSell(amounts[0],sellAmounts[sellAmounts.length-1]);
+        }
 }
